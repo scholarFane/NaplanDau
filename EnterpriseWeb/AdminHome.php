0e61658b68b67be32e92cfef5ecefb2b27c1a9a0
Cactus635 <57930441+Cactus635@users.noreply.github.com>
2021-02-24 13:30:04 +0700

    making the home pages a little bit dynamic
    when login as a role of user , show the text line " [user_role] [user_name] here"

--- EnterpriseWeb/AdminHome.php
+++ EnterpriseWeb/AdminHome.php
@@ -1,1 +1,22 @@
-<h1>Admin page here</h1>
+<?php 
+	session_start();
+	include("DatabaseConfig/dbConfig.php");
+
+	if(!isset($_SESSION['id'])){
+		echo "<script>window.open('login.php','_self')</script>";
+
+	}else{
+		$user_session = $_SESSION['id'];
+
+		$get_user = "select * from user where username = '$user_session'";
+		$run_user = mysqli_query($conn,$get_user);
+		$row_user = mysqli_fetch_array($run_user);
+
+		$user_name = $row_user['username'];
+		$user_role = $row_user['user_role'];
+	
+ ?>
+<h1><?php echo $user_role," ", $user_name, " here" ?></h1>
+<a href="logout.php">Log out</a>
+
+<?php } ?>
